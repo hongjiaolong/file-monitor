383b5d0d2b1c1a8f86eddfb4b40cea58959297bc
hongjiaolong <hongjiaolong@192.168.18.132>
2017-12-15 08:03:28 +0800

    sf

---
+++ src/main/java/com/gan/monitor/IWatchEventHandler.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright(C) 2013 Xxx Corporation. All rights reserved.
+ * 
+ * Contributors:
+ *     Xxx Corporation - initial API and implementation
+ */
+package com.gan.monitor;
+
+/**
+ * 
+ *
+ * @author hongjiaolong
+ * @date 2017年12月14日 下午7:48:45
+ * @version 1.0
+ *
+ */
+public interface IWatchEventHandler {
+    void handle();
+}
